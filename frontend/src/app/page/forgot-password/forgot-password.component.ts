589110c584c524335aca4e11c38018ba4f0f1a16
levi <kovacsl6@kkszki.hu>
2023-01-10 12:44:30 +0100

    Forgot password view

--- frontend/src/app/page/forgot-password/forgot-password.component.ts
+++ frontend/src/app/page/forgot-password/forgot-password.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class ForgotPasswordComponent {
 
+  emailClass: string = '';
+  selectedElement: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  focusToggler(event: Event, className: string): string {
+    event.type == 'focus' ? className= 'focused' : className='';
+
+    if(event) {
+      this.selectedElement = event.target;
+    } else {
+      this.selectedElement = null;
+    }
+
+    if (this.selectedElement.value != '') {
+      className= 'focused';
+    }
+    return className;
+  }
+
+  emailFocusToggler(event: Event): void {
+    this.emailClass = this.focusToggler(event, this.emailClass)
+  }
 }
