76bf1ed4c22bf4b41725f498d0918d0083d0b75f
DiSLucky7 <nagyklevi@gmail.com>
2023-01-28 18:26:28 +0100

    Minor fixes on navbar, user-list, host script, env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  // // apiUrl: 'http://localhost:3000/'
-  apiUrl: 'http://192.168.50.17:3000/'
+  // apiUrl: 'http://localhost:3000/'
+  // apiUrl: 'http://192.168.50.17:3000/'
+  apiUrl: 'http://192.168.1.102:3000/'
 };
 
 /*
