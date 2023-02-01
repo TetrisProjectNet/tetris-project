e6c5394f258fd53d541b7af8d3890181aac0f4e4
DiSLucky7 <nagyklevi@gmail.com>
2023-02-01 20:39:30 +0100

    Sorter pipe extended, All & Avg leaderboards added

--- frontend/src/app/pipe/sorter.pipe.ts
+++ frontend/src/app/pipe/sorter.pipe.ts
@@ -5,17 +5,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SorterPipe implements PipeTransform {
 
-  transform(value: any[] | null, column: string, direct: boolean, length?: number): any[] | null {
+  transform(value: any[] | null, column: string, direct: boolean, length?: number, type?: string): any[] | null {
     if (!Array.isArray(value) || !column) {
       return value;
     }
 
     if (column === 'scores' && length) {
-      value.map((item) => {
-        item[column] = item[column].sort((a: any, b: any) => {
-          b - a;
-        })[0];
-      });
+
+      // single game
+      if (type === 'single') {
+        value.map((item) => {
+          item[column] = item[column].sort((a: any, b: any) => {
+            b - a;
+          })[0];
+        });
+      }
+      // all games
+      if (type === 'all') {
+        value.map((item) => {
+          item[column] = item[column].reduce((a: any, b: any) => a + b, 0);
+        });
+      }
+      // avg of games
+      if (type === 'avg') {
+        value.map((item) => {
+          const sum = item[column].reduce((a: any, b: any) => a + b, 0);
+          const num = (sum / item[column].length) || 0;
+          item[column] = Math.round((num + Number.EPSILON) * 100) / 100;
+        });
+      }
+
       return value.sort((a: any, b: any) => {
         // equal items sort equally
         if (a[column] === b[column]) {
@@ -54,11 +73,3 @@ export class SorterPipe implements PipeTransform {
     });
   }
 }
-
-// // nulls sort after anything else
-// if (a[column] === null) {
-//   return 1;
-// }
-// if (b[column] === null) {
-//   return -1;
-// }
