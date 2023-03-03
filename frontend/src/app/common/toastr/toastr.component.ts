569aef29cc19f2da6af5041df9743e045d227712
levi <kovacsl6@kkszki.hu>
2023-03-03 11:36:03 +0100

    Trying to have dif. animation on mobile, row label

--- frontend/src/app/common/toastr/toastr.component.ts
+++ frontend/src/app/common/toastr/toastr.component.ts
@@ -1,5 +1,5 @@
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { faCircleCheck, faCircleXmark, } from '@fortawesome/free-regular-svg-icons';
 import { faCheck, faCircleExclamation, faCircleInfo, faSquareCheck, faXmark,  faInfo, faExclamation } from '@fortawesome/free-solid-svg-icons';
 import { Toast, ToastPackage, ToastrService } from 'ngx-toastr';
@@ -15,39 +15,50 @@ import { Toast, ToastPackage, ToastrService } from 'ngx-toastr';
       })),
       transition('inactive <=> active', animate('500ms ease-out', keyframes([
         style({
-          transform: 'translateX(340px)',
+          transform: '{{translateType}}(340px)',
           offset:0,
           opacity: 0,
         }),
         style({
           offset:.7,
           opacity: 1,
-          transform: 'translateX(-20px)'
+          transform: '{{translateType}}(-20px)'
         }),
         style({
           offset: 1,
-          transform: 'translateX(0)',
+          transform: '{{translateType}}(0)',
         })
       ]))),
-      transition('active => removed', animate('500ms ease-in', keyframes([
-        style({
-          transform: 'translateX(-20px)',
-          opacity: 1,
-          offset: .2
-        }),
-        style({
-          opacity:0,
-          transform: 'translateX(340px)',
-          offset: 1
-        })
-      ])))
+      // transition('active => removed', animate('500ms ease-in', keyframes([
+      //   style({
+      //     transform: '{{translateType}}(-20px)',
+      //     opacity: 1,
+      //     offset: .2
+      //   }),
+      //   style({
+      //     opacity:0,
+      //     transform: '{{translateType}}(340px)',
+      //     offset: 1
+      //   })
+      // ])))
     ]),
+
+    // trigger('slideInOut', [
+    //   transition(':enter', [
+    //     style({ transform: '{{translateType}}(-100%)' }),
+    //     animate('900ms ease-in', style({ transform: '{{translateType}}(0%)' })),
+    //   ]),
+    //   transition(':leave', [animate('700ms ease-in', style({ transform: '{{translateType}}(-100%)' }))]),
+    // ]),
   ],
   preserveWhitespaces: false,
 })
+
 export class ToastrComponent extends Toast {
   // used for demo purposes
-  undoString = 'undo';
+  // undoString = 'undo';
+
+  @ViewChild('toastr') toastr!: ElementRef;
 
   faSquareCheck = faSquareCheck;
   faCircleCheck = faCircleCheck;
@@ -59,6 +70,37 @@ export class ToastrComponent extends Toast {
   faCheck = faCheck;
   faXmark = faXmark;
 
+  screenWidth: number = 0;
+  isActive: string = 'inactive';
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+    // console.log(this.screenWidth);
+  }
+
+  ngOnInit(): void {
+    // this.screenWidth = window.innerWidth;
+    console.log('asd');
+    this.onResize();
+    // this.isActive = 'active';
+  }
+
+  ngAfterViewInit() {
+    console.log(this.toastr);
+    setTimeout(() => {
+      this.isActive = (this.isActive =='inactive') ? 'active' : 'inactive';
+    })
+  }
+
+
+  getAnimationTransformValue(): string {
+    // console.log('asdasd');
+    
+    return this.screenWidth <= 800 ? 'translateY' : 'translateX';
+  }
+
+
   // constructor is only necessary when not using AoT
   // constructor(
   //   protected toastrService: ToastrService,
@@ -67,10 +109,10 @@ export class ToastrComponent extends Toast {
   //   super(toastrService, toastPackage);
   // }
 
-  action(event: Event) {
-    event.stopPropagation();
-    this.undoString = 'undid';
-    this.toastPackage.triggerAction();
-    return false;
-  }
+  // action(event: Event) {
+  //   event.stopPropagation();
+  //   this.undoString = 'undid';
+  //   this.toastPackage.triggerAction();
+  //   return false;
+  // }
 }
