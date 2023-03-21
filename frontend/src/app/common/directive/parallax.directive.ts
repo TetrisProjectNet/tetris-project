1dfc4102dece938e43e06a37279ee4e63cd95933
levi <kovacsl6@kkszki.hu>
2023-03-21 12:26:55 +0100

    Working on home page with parallax directive

--- frontend/src/app/common/directive/parallax.directive.ts
+++ frontend/src/app/common/directive/parallax.directive.ts
@@ -10,6 +10,7 @@ export class ParallaxDirective {
   }
 
   private factor: number = 0;
+  true: boolean = true;
 
   constructor(
     private elementRef: ElementRef,
@@ -18,13 +19,35 @@ export class ParallaxDirective {
 
   @HostListener('window:scroll')
   onWindowScroll() {
-    this.renderer.setProperty(
-      this.elementRef.nativeElement, 
-      'style',
-      `transform: translateY(${this.getTranslation()}px)`);
+    // console.log('top: ', this.calcTopThreshold());
+    console.log(this.elementRef.nativeElement.getBoundingClientRect());
+    // console.log(window.innerHeight);
+    // this.calcTopThreshold() > 0
+    if (this.elementRef.nativeElement.getBoundingClientRect().top < window.innerHeight) {
+      this.renderer.setProperty(
+        this.elementRef.nativeElement, 
+        'style',
+        `transform: translateY(${this.getTranslation()}px)`
+      );
+
+    }
   }
 
   private getTranslation() {
     return window.scrollY * this.factor / 10;
+  }
+
+  private calcTopThreshold() {
+    return (
+      this.elementRef.nativeElement.getBoundingClientRect().top + 
+      this.elementRef.nativeElement.getBoundingClientRect().height
+    )
+  }
+
+  private calcBottomThreshold() {
+    return (
+      this.elementRef.nativeElement.getBoundingClientRect().top + 
+      this.elementRef.nativeElement.getBoundingClientRect().height
+    )
   }
 }
