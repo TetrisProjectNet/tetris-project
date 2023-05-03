5543f4f7a8b5c8186a5235df8186f7d42a541fb1
DiSLucky7 <nagyklevi@gmail.com>
2023-05-03 23:06:48 +0200

    Frontend published on firebase - version 0.2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://localhost:7041/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBuo77yHfkMPOzYfePtNQfOkRDJvNS0QWc",
+    authDomain: "tetris-project-net-1.firebaseapp.com",
+    projectId: "tetris-project-net-1",
+    storageBucket: "tetris-project-net-1.appspot.com",
+    messagingSenderId: "498840282506",
+    appId: "1:498840282506:web:b75f7ea13b9b63118fd473"
+  }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,7 +7,15 @@ export const environment = {
   // apiUrl: 'http://localhost:3000/'
   // apiUrl: 'http://192.168.50.17:3000/'
   // apiUrl: 'http://192.168.1.101:3000/'
-  apiUrl: 'https://localhost:7041/'
+  apiUrl: 'https://localhost:7041/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBuo77yHfkMPOzYfePtNQfOkRDJvNS0QWc",
+    authDomain: "tetris-project-net-1.firebaseapp.com",
+    projectId: "tetris-project-net-1",
+    storageBucket: "tetris-project-net-1.appspot.com",
+    messagingSenderId: "498840282506",
+    appId: "1:498840282506:web:b75f7ea13b9b63118fd473"
+  }
 };
 
 /*
