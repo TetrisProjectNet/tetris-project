075f58086ab720c3f3e201a1bbc90284560df887
Hunter99011 <github@hunter99011>
2023-05-04 04:45:46 +0200

    Merge branch 'milan'

---
+++ desktop/Tetris/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tetris.Models {
+    public class User {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("password")]
+        public string? Password { get; set; }
+
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [JsonPropertyName("banned")]
+        public bool Banned { get; set; }
+
+        [JsonPropertyName("joinDate")]
+        public string? JoinDate { get; set; }
+
+        [JsonPropertyName("lastOnlineDate")]
+        public string? LastOnlineDate { get; set; }
+
+        [JsonPropertyName("coins")]
+        public int Coins { get; set; }
+
+        [JsonPropertyName("shopItems")]
+        public ShopItem[]? ShopItems { get; set; }
+
+        [JsonPropertyName("scores")]
+        public int[]? Scores { get; set; }
+
+        [JsonPropertyName("friends")]
+        public int[]? Friends { get; set; }
+
+        public User(string id, string username, string password, string email, string role, bool banned, string joinDate,
+                    string lastOnlineDate, int coins, ShopItem[] shopItems, int[] scores, int[] friends) {
+            Id = id;
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            Banned = banned;
+            JoinDate = joinDate;
+            LastOnlineDate = lastOnlineDate;
+            Coins = coins;
+            ShopItems = shopItems;
+            Scores = scores;
+            Friends = friends;
+        }
+    }
+}
---
+++ desktop/Tetris/SingletonHook.cs
@@ -0,0 +1,20 @@
+﻿using SharpHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class SingletonHook {
+
+        private static readonly Lazy<TaskPoolGlobalHook> lazy =
+            new Lazy<TaskPoolGlobalHook>(() => new TaskPoolGlobalHook());
+
+        public static TaskPoolGlobalHook Instance { get { return lazy.Value; } }
+
+        private SingletonHook() {
+        }
+    }
+}
